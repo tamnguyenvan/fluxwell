12e5791bf63a54c57df2ca155a3f259ba112baa2
Nguyen Van Tam <tamnvhustcc@gmail.com>
2024-08-06 09:26:21 +0700

    Modal setup command

--- api/index.py
+++ api/index.py
@@ -1,16 +1,13 @@
-import os
 import re
 import io
-import time
 import base64
 import uuid
 import threading
 import subprocess
 
-from flask import Flask, g, request, jsonify, Response, stream_with_context
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 import requests
-from PIL import Image
 
 app = Flask(__name__)
 CORS(app)
@@ -19,39 +16,23 @@ app.config["MAX_CONTENT_LENGTH"] = 16 * 1024 * 1024
 
 images_dir = "./images"
 tasks = {}
-
-def get_deployed_url():
-    if "deployed_url" not in g:
-        g.deployed_url = None
-    return g.deployed_url
-
-
-def set_deployed_url(url):
-    g.deployed_url = url
+deployed_url = ""
 
 
 @app.route("/api/deploy", methods=["POST"])
 def deploy():
     try:
         data = request.get_json()
-        token_id = data.get("modal_token_id")
-        token_secret = data.get("modal_token_secret")
-        print(token_id, token_secret)
-
-        if not token_id or not token_secret:
-            return jsonify({"status": "failed", "message": "Missing `token_id` or `token_secret`"}), 400
-
-        os.environ["MODAL_TOKEN_ID"] = token_id
-        os.environ["MODAL_TOKEN_SECRET"] = token_secret
+        access_token = data.get("access_token")
+        result = subprocess.run(['modal', 'setup'], capture_output=True, text=True, check=True)
         result = subprocess.run(["modal deploy main.py"], capture_output=True, text=True, check=True, shell=True)
 
         pattern = r"Created web function .* => (https://[^\s]+)"
         match = re.search(pattern, result.stdout)
 
         if match:
-            url = match.group(1)
-            set_deployed_url(url)
-            return jsonify({"status": "success", "url": url}), 200
+            deployed_url = match.group(1)
+            return jsonify({"status": "success", "url": deployed_url}), 200
         else:
             return jsonify({"status": "success", "message": "Deployment successful, but no URL found in output"}), 200
     except Exception as e:
@@ -60,7 +41,6 @@ def deploy():
 
 
 def generate_image(task_id, prompt, backend_url):
-    # url = "https://tamnguyenvan--flux-model-web-inference.modal.run"
     params = {"prompt": prompt}
     try:
         print(f"Backend: {backend_url} prompt: {prompt}")
@@ -116,6 +96,7 @@ def generate():
     except Exception as e:
         print("Error", e)
         return jsonify({"status": "failed", "message": f"Error: {str(e)}"}), 500
+
 
 @app.route("/api/task-status/<task_id>", methods=["GET"])
 def task_status(task_id):
